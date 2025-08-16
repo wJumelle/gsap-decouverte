fb8a58ae6ac50812f934544a5b307ad7465ca2c9
wJumelle <wjumelle@gmail.com>
2025-08-16 18:06:23 +0200

    [TASK] Getting Started > Méthodes > gsap.fromTo

--- gsap-discovery/src/scripts/anim_1.js
+++ gsap-discovery/src/scripts/anim_1.js
@@ -6,14 +6,20 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('gsap loaded ?', {gsap});
 
     // Animation 1
-    const tween_1 = gsap.to(".animation--1 .square", { x: 600 });
+    const tween_1 = gsap.to(".animation--1 .square", { x: 600, paused: true });
     document.getElementById('anim_1_button').addEventListener('click', () => {
         tween_1.restart();
     });
 
     // Animation 2
-    const tween_2 = gsap.from(".animation--2 .square", { x: 600 });
+    const tween_2 = gsap.from(".animation--2 .square", { x: 600, paused: true });
     document.getElementById('anim_2_button').addEventListener('click', () => {
         tween_2.restart();
+    });
+
+    // Animation 3
+    const tween_3 = gsap.fromTo(".animation--3 .square", { x: 0, paused: true }, { x: 600, paused: true });
+    document.getElementById('anim_3_button').addEventListener('click', () => {
+        tween_3.restart();
     });
 });
