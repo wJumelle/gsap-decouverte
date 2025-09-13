dbeae2b7c40cfc8a9d30cce2b4e9ce1059459fae
wJumelle <wjumelle@gmail.com>
2025-09-13 19:03:29 +0200

    [TASK] Stagger > Présentation inversée

--- gsap-discovery/src/scripts/anim_2.js
+++ gsap-discovery/src/scripts/anim_2.js
@@ -6,8 +6,16 @@ window.addEventListener('DOMContentLoaded', () => {
     console.log('gsap loaded ?', {gsap});
 
     // Animation 1 - Présentation
-    const tween_1 = gsap.to('.animation--1 .square', { paused: true, x: 600, stagger: 0.25 });
-    document.getElementById('anim_1_button').addEventListener('click', () => {
-        tween_1.restart();
+    const tween_1_1 = gsap.to('.animation--1 .square', { paused: true, x: 600, stagger: 0.25 });
+    const tween_1_2 = gsap.to('.animation--1 .square', { paused: true, x: 600, stagger: -0.25 });
+    document.getElementById('anim_1_1_button').addEventListener('click', () => {
+        tween_1_2.pause();
+        gsap.set('.animation--1 .square', { x: 0 });
+        tween_1_1.restart();
+    });
+    document.getElementById('anim_1_2_button').addEventListener('click', () => {
+        tween_1_1.pause();
+        gsap.set('.animation--1 .square', { x: 0 });
+        tween_1_2.restart();
     });
 });
